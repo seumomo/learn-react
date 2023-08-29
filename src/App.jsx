972e53ff1247d6b3fd0a05b9b9f2ce790909c96c
seumomo <xo0207@naver.com>
2023-08-29 17:34:28 +0900

    [Add] Merge 전 Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import AuthProvider from '@/contexts/Auth';
 import ThemeProvider from '@/contexts/Theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
+
+const queryClient = new QueryClient();
+
+console.log(queryClient);
 
 function App() {
   return (
@@ -11,9 +17,12 @@ function App() {
       <HelmetProvider>
         <ThemeProvider>
           <AuthProvider>
-            <div className="App">
-              <RouterProvider router={router} />
-            </div>
+            <QueryClientProvider client={queryClient}>
+              <div className="App">
+                <RouterProvider router={router} />
+              </div>
+              <ReactQueryDevtools />
+            </QueryClientProvider>
           </AuthProvider>
         </ThemeProvider>
       </HelmetProvider>
--- src/learn/14-tanstack-query.jsx
+++ src/learn/14-tanstack-query.jsx
@@ -1,34 +1,42 @@
 import Spinner from '@/components/Spinner';
 import { numberWithComma } from '@/utils';
+import { useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 
 function TanStackQueryLibrary() {
-  const [isLoading, setIsLoading] = useState(null);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState(null);
+  // const [isLoading, setIsLoading] = useState(null);
+  // const [error, setError] = useState(null);
+  // const [data, setData] = useState(null);
 
-  useEffect(() => {
-    setIsLoading(true);
+  // useEffect(() => {
+  //   setIsLoading(true);
 
-    async function fetchProductsData(page = 1, perPage = 50) {
-      try {
-        const response = await fetch(
-          `https://pb-demo-app.pockethost.io/api/collections/products/records?page=${page}&perPage=${perPage}`
-        );
-        const json = await response.json();
-        setData(json);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    }
+  //   async function fetchProductsData(page = 1, perPage = 50) {
+  //     try {
+  //       const response = await fetch(
+  //         `https://pb-demo-app.pockethost.io/api/collections/products/records?page=${page}&perPage=${perPage}`
+  //       );
+  //       const json = await response.json();
+  //       setData(json);
+  //     } catch (error) {
+  //       setError(error);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   }
 
-    fetchProductsData();
-  }, []);
+  //   fetchProductsData();
+  // }, []);
 
-  const isError = !!error;
+  // const isError = !!error;
+
+  useQuery({
+    queryKey: ['products'],
+    queryFn: async () => {
+      const response = await fetch();
+    },
+  });
 
   if (isLoading) {
     return (
@@ -51,23 +59,20 @@ function TanStackQueryLibrary() {
         Tanstack(React) Query 라이브러리 활용
       </h2>
       <ul className="grid grid-cols-3 gap-2">
-        {data?.items?.map((item) => {
-          console.log(item);
-          return (
-            <li key={item.id} className="flex flex-col gap-1 my-4">
-              <strong>{item.title}</strong>
-              <img
-                src={
-                  !item.photo
-                    ? 'https://placehold.co/191x291?text=PHOTO'
-                    : `https://pb-demo-app.pockethost.io/api/files/${item.collectionId}/${item.id}/${item.photo}`
-                }
-                alt=""
-              />
-              <span>{numberWithComma(item.price)}</span>
-            </li>
-          );
-        })}
+        {data?.items?.map((item) => (
+          <li key={item.id} className="flex flex-col gap-1 my-4">
+            <strong>{item.title}</strong>
+            <img
+              src={
+                !item.photo
+                  ? 'https://placehold.co/191x291?text=PHOTO'
+                  : `https://pb-demo-app.pockethost.io/api/files/${item.collectionId}/${item.id}/${item.photo}`
+              }
+              alt=""
+            />
+            <span>{numberWithComma(item.price)}</span>
+          </li>
+        ))}
       </ul>
     </div>
   );
